e0c853279b7969c0a94adc32c3006935d4516150
Xander P. <ajamespickering@gmail.com>
2026-02-24 20:08:12 -0700

    Trying to make an RB-based character controller

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 
 @export var mouse_sensitivity: float = 0.33
+@export var walk_speed: float = 4.0
+@export var sprint_speed: float = 6.5
 
 @onready var head: Node3D = $Head
 
@@ -10,6 +12,8 @@ var mouse_locked: bool = true:
 		_update_mouselock()
 var pitch: float = 0.0
 var yaw: float = 0.0
+var move_speed: float = 0.0
+var wish_dir: Vector2 = Vector2.ZERO
 
 
 func _ready() -> void:
@@ -40,3 +44,11 @@ func _process(delta: float) -> void:
 	
 	pitch = 0.0
 	yaw = 0.0
+
+
+func _physics_process(_delta: float) -> void:
+	wish_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	move_speed = sprint_speed if Input.is_action_pressed("move_sprint") else walk_speed
+	
+	var move_vector = Vector3(wish_dir.x, 0.0, wish_dir.y) * move_speed
+	apply_force(move_vector)
