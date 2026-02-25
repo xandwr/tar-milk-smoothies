b620865e4c6e477f31078272cb2037e0243a358e
Xander P. <ajamespickering@gmail.com>
2026-02-24 20:37:57 -0700

    CharacterBody3D, FUCK rigidbody movement man

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,13 @@
-extends RigidBody3D
+extends CharacterBody3D
 
 @export var mouse_sensitivity: float = 0.33
 @export var walk_speed: float = 3.5
 @export var sprint_speed: float = 5.5
 @export var jump_force: float = 4.0
+@export var acceleration: float = 32.0
+@export var air_acceleration: float = 4.0
 
 @onready var head: Node3D = $Head
-@onready var ground_cast: ShapeCast3D = $GroundCast
 
 var mouse_locked: bool = true:
 	set(value):
@@ -14,8 +15,7 @@ var mouse_locked: bool = true:
 		_update_mouselock()
 var pitch: float = 0.0
 var yaw: float = 0.0
-var move_speed: float = 0.0
-var wish_dir: Vector2 = Vector2.ZERO
+var external_velocity: Vector3 = Vector3.ZERO
 
 
 func _ready() -> void:
@@ -26,16 +26,14 @@ func _update_mouselock() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if mouse_locked else Input.MOUSE_MODE_VISIBLE
 
 
-func is_grounded() -> bool:
-	if ground_cast and ground_cast.is_colliding():
-		return true
-	return false
+func apply_impulse(impulse: Vector3) -> void:
+	external_velocity += impulse
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		mouse_locked = not mouse_locked
-	
+
 	if event is InputEventMouseMotion:
 		pitch -= event.relative.y
 		yaw -= event.relative.x
@@ -43,23 +41,24 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	if head and mouse_locked:
-		head.rotation.x = clampf(
-			head.rotation.x + (pitch * delta * mouse_sensitivity),
-			-PI / 2.0,
-			PI / 2.0
-		)
+		head.rotation.x = clampf(head.rotation.x + (pitch * delta * mouse_sensitivity), -PI / 2.0, PI / 2.0)
 		rotation.y += yaw * delta * mouse_sensitivity
-	
 	pitch = 0.0
 	yaw = 0.0
-	
-	if is_grounded() and Input.is_action_just_pressed("move_jump"):
-		apply_impulse(Vector3(0, jump_force * mass, 0))
-	
-	wish_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	move_speed = sprint_speed if Input.is_action_pressed("move_sprint") else walk_speed
-	
+
+	if not is_on_floor(): velocity.y += get_gravity().y * delta
+	if is_on_floor() and Input.is_action_just_pressed("move_jump"): velocity.y = jump_force
+
+	var wish_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var move_speed = sprint_speed if Input.is_action_pressed("move_sprint") else walk_speed
 	var direction = (transform.basis * Vector3(wish_dir.x, 0.0, wish_dir.y)).normalized()
-	var target_velocity = direction * move_speed
-	var velocity_diff = target_velocity - Vector3(linear_velocity.x, 0.0, linear_velocity.z)
-	apply_central_force(velocity_diff * mass * 32.0)
+	var target = direction * move_speed
+	var accel = acceleration if is_on_floor() else air_acceleration
+
+	velocity.x = move_toward(velocity.x, target.x, accel * delta)
+	velocity.z = move_toward(velocity.z, target.z, accel * delta)
+
+	velocity += external_velocity
+	external_velocity = Vector3.ZERO
+
+	move_and_slide()
